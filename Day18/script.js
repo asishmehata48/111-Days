5fa6da44a1a4579961a70b1e64fa49dd7edd86fc
Asish Mehata <mehataasish@gmail.com>
2025-05-01 22:40:08 +0545

    Function in JS

---
+++ Day18/script.js
@@ -0,0 +1,16 @@
+// Function declaration and calling
+function greet(name) {
+    console.log("Hello, " + name + "!");
+}
+greet("World");
+
+// Function with parameters and return value
+function add(a, b) {
+    return a + b;
+}
+let sum = add(3, 4);
+console.log("Sum:", sum);
+
+// Arrow function
+const multiply = (x, y) => x * y;
+console.log("Product:", multiply(5, 6));
