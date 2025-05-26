d6f392eeff11f9e466e078a3d318f79a4a10f88d
Asish Mehata <mehataasish@gmail.com>
2025-05-26 22:54:12 +0545

    writing on the code, first component

---
+++ react-app/src/App.jsx
@@ -0,0 +1,10 @@
+import "./App.css"
+function Welcome() {
+  return (
+    <div>
+      <h1>Welcome to My React App 🚀</h1>
+      <p>This is my first React component, built using JSX!</p>
+    </div>
+  );
+}
+export default App;
