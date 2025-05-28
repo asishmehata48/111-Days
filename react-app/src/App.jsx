b855b2862ca56cfd666fc3c3a6f4d5c0c5289174
Asish Mehata <mehataasish@gmail.com>
2025-05-28 09:29:36 +0545

    modified

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,5 +1,21 @@
 import "./App.css"
-function App() {
-  return <button>Hi there</button>
+
+function Title(){
+  return <h1>I'm the title</h1>
 }
+
+
+function App() {
+  const appTitle = "React Concepts Demo";
+  const appMessage = "Using React Fragment, JSX curly braces, structured components, and styled components.";
+
+  return (
+    <>
+      <Header title={appTitle} />
+      <Content message={appMessage} />
+    </>
+  );
+}
+
 export default App;
+
