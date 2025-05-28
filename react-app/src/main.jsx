cdfbbdda90307e6c6f5d850ae432d7c9fefa0a9f
Asish Mehata <mehataasish@gmail.com>
2025-05-28 09:29:47 +0545

    changed.

--- react-app/src/main.jsx
+++ react-app/src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
