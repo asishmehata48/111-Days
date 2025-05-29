70cc25e164f280cbd5e7559739dd7d2f6c61908e
Asish Mehata <mehataasish@gmail.com>
2025-05-29 23:18:21 +0545

    dynamic component styling

--- react-app/src/Product.jsx
+++ react-app/src/Product.jsx
@@ -1,11 +1,13 @@
 import './Product.css';
 
 function Product({title, price}) {
+    let isDiscount = price > 30000;
+    let styles ={backgroundColor : isDiscount ? "lightPink": null}
     return (
-        <div className="Product">
+        <div className="Product" style={styles}>
             <h3>{title}</h3>
             <h5>Price : {price}</h5>
-            {price > 30000 && <p>Discount of 5%</p> }
+            {isDiscount && <p>Discount of 5%</p> }
         </div>
     );
 }
