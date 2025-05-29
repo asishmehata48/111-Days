19784f812972d742a6249ae62caae04f422a4f9f
Asish Mehata <mehataasish@gmail.com>
2025-05-29 22:45:31 +0545

    rendering arrays.

--- react-app/src/ProductTab.jsx
+++ react-app/src/ProductTab.jsx
@@ -1,11 +1,10 @@
 import Product from './Product.jsx';
 
 function ProductTab() {
-    let options=["hi-tech", "durable", "efficient"];
-    let options2 = {a: "hi-tech", b: "durable", c: "efficient"}
+    let Options= ["hi-tech", "durable", "efficient"];
     return (
     <>
-        <Product title="Phone" price={30000} features={options} features2={["hi-tech", "durable", "efficient"]}/>
+        <Product title="Phone" price={30000} features={Options}/>
         {/* <Product title="Laptop" price={80000} />
         <Product title="Camera"  price={1} /> */}
     </>
