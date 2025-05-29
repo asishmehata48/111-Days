75a08b97bde373509f15e53df9245a96b6c826b5
Asish Mehata <mehataasish@gmail.com>
2025-05-29 16:33:35 +0545

    components structured

---
+++ react-app/src/Product.jsx
@@ -0,0 +1,10 @@
+function Product() {
+    return (
+        <>
+        <h3>This is my Product Title</h3>
+        <h5>This is gonna be the Product Description</h5>
+        </>
+    );
+}
+
+export default Product;
