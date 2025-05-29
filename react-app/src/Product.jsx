a944b2c78ca7efe5e5bf92b9731ce6991e8005fa
Asish Mehata <mehataasish@gmail.com>
2025-05-29 22:23:55 +0545

    passing arrays to Props

--- react-app/src/Product.jsx
+++ react-app/src/Product.jsx
@@ -1,10 +1,12 @@
 import './Product.css';
 
-function Product({title, price=1}) {
+function Product({title, price, features, features2}) {
     return (
         <div className="Product">
             <h3>{title}</h3>
             <h5>Price : {price}</h5>
+            <p>{features}</p>
+            <p>{features2}</p>
         </div>
     );
 }
--- react-app/src/ProductTab.jsx
+++ react-app/src/ProductTab.jsx
@@ -1,11 +1,13 @@
 import Product from './Product.jsx';
 
 function ProductTab() {
+    let options=["hi-tech", "durable", "efficient"];
+    let options2 = {a: "hi-tech", b: "durable", c: "efficient"}
     return (
     <>
-        <Product title="Phone" price={30000} />
-        <Product title="Laptop" price={80000}/>
-        <Product title="Camera"  />
+        <Product title="Phone" price={30000} features={options} features2={["hi-tech", "durable", "efficient"]}/>
+        {/* <Product title="Laptop" price={80000} />
+        <Product title="Camera"  price={1} /> */}
     </>
     );
 }
