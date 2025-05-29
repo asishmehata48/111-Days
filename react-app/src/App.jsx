04dc98117bcbf72209b9b1ddcd168e794b64a4b6
Asish Mehata <mehataasish@gmail.com>
2025-05-29 23:31:05 +0545

    activity done.

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -2,9 +2,17 @@ import './App.css';
 import Title from './Title.jsx';
 import Product from './Product.jsx';
 import ProductTab from './ProductTab.jsx';
+import MessageBox from './MessageBox.jsx';
 
 function App() {
-  return <ProductTab />;
+  return (
+    <>
+    <MessageBox userName="AsishMehata" textColor="Yellow"/>
+    <MessageBox userName="AarohiMehata" textColor="Blue"/>
+    <MessageBox userName="ArushiMehata" textColor="Orange"/>
+    <ProductTab />
+    </>
+  );
 }
 
 export default App;
