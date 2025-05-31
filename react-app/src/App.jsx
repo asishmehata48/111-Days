7cf3e560a26554c45993b87a865937a11a00df8b
Asish Mehata <mehataasish@gmail.com>
2025-05-31 16:14:16 +0545

    Button.jsx & Form.jsx imported

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -2,13 +2,14 @@ import './App.css';
 import Title from './Title.jsx';
 import Product from './Product.jsx';
 import ProductTab from './ProductTab.jsx';
-// import MessageBox from './MessageBox.jsx';
+import Button from './Button.jsx';
+import MessageBox from './MessageBox.jsx';
+import Form from './Form.jsx';
 
 function App() {
   return (
     <>
-    <h2>Blockbuster Deals on Computer Accessories | Shop Now</h2>
-    <ProductTab />
+    <Form />
     </>
   );
 }
