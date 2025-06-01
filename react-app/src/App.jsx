b48c049fc3193ef3a2cef653d7fbd8ddc2cf38c7
Asish Mehata <mehataasish@gmail.com>
2025-06-01 23:42:05 +0545

    imported Counter.jsx

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -5,11 +5,13 @@ import ProductTab from './ProductTab.jsx';
 import Button from './Button.jsx';
 import MessageBox from './MessageBox.jsx';
 import Form from './Form.jsx';
+import Counter from './Counter.jsx';
 
 function App() {
   return (
     <>
-    <Form />
+    <h2>State in React</h2>
+    <Counter />
     </>
   );
 }
